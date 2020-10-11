a4d8c14b548963594f04a37d31feeaa0359de72a
Evgeny Groshev <webdev.yeisk@yandex.ru>
2020-10-11 15:37:15 +0300

    products

--- #src/js/script.js
+++ #src/js/script.js
@@ -7,6 +7,7 @@ $(function () {
 
 
 	const slider = document.querySelector('.swiper-container');
+
 	let mySwiper = new Swiper(slider, {
 		slidesPerView: 3,
 		spaceBetween: 23,
@@ -39,4 +40,7 @@ $(function () {
 			}
 		}
 	});
+
+
+
 });
